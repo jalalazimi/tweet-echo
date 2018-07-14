e123f2d44a154a985d602aa3b216a619f7366e0b
Jalal <jalalazimi@gmail.com>
2018-07-15 00:33:53 +0430

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+import './style';
+import { Component } from 'preact';
+
+export default class App extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
