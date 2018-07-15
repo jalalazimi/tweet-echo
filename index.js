9dc9dad0adec0cb48c32eac83e036665f402b84c
Jalal <jalalazimi@gmail.com>
2018-07-15 11:49:14 +0430

    fetch tweet by id

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ import axios from 'axios';
 
 const sanitizeHtml = require('sanitize-html');
 
-
-// const twitter = new Twitter({
-//   consumer_key: 'uloHq83EUIjx32TSnqJUTWTXt',
-//   consumer_secret: 'FODNE2TyueQEDuwhPRUmbWyniKQgtCQKbbsjEddtLFMACRbFoM',
-//   access_token_key: '16341288-Z9c468O0OC3881egpFRN0i3yxDDakfCVTZ2UCLOWb',
-//   access_token_secret: 'wOuH1AKqiTazMLehaNvkLhZ8QHFmx755mKZXa69KhWE23',
-// })
-
 export default class App extends Component {
 
   state = {
@@ -21,10 +13,7 @@ export default class App extends Component {
   componentDidMount() {
     axios({
       method: 'GET',
-      url: 'https://api.twitter.com/1.1/statuses/show.json?id=762207487395188736',
-      headers: {
-        "Authorization": "OAuth oauth_consumer_key=\\\"uloHq83EUIjx32TSnqJUTWTXt\\\",oauth_token=\\\"16341288-Z9c468O0OC3881egpFRN0i3yxDDakfCVTZ2UCLOWb\\\",oauth_signature_method=\\\"HMAC-SHA1\\\",oauth_timestamp=\\\"1531602464\\\",oauth_nonce=\\\"vEQII973SeT\\\",oauth_version=\\\"1.0\\\",oauth_signature=\\\"inmKBk46O2XzF7wLPn8M1wP6d4U%3D\\\"",
-      }
+      url: 'http://localhost:8523',
     })
       .then((res) => {
         console.log('this.clean(res.data.html)', res);
@@ -47,7 +36,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.state.tweet && this.clean(this.state.tweet.html)}</h1>
+
+        <h1>{this.state.tweet && this.clean(this.state.tweet)}</h1>
       </div>
     );
   }
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const request = require('request-promise');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://api.twitter.com/1.1/statuses/show.json',
+    qs: {id: '762207487395188736'},
+    headers:
+      {
+        'Cache-Control': 'no-cache',
+        Authorization: 'OAuth oauth_consumer_key="uloHq83EUIjx32TSnqJUTWTXt",oauth_token="16341288-Z9c468O0OC3881egpFRN0i3yxDDakfCVTZ2UCLOWb",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1531603149",oauth_nonce="KTGNsu4i3NU",oauth_version="1.0",oauth_signature="UOXmJP3Jpsma8wK9WQGDiGVgJdw%3D"'
+      }
+  };
+  request(options)
+    .then(body => {
+      res.json(JSON.parse(body));
+    })
+    .catch(function (err) {
+      throw new Error(err);
+    });
+})
+
+app.listen(8523, () => console.log('Example app listening on port 3000!'))
